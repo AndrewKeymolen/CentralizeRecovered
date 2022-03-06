fa9339bb3bef4792f1672c0b3052ff57ea125de0
Andrew Keymolen <andrewkeymolen@gmail.com>
2022-03-06 16:17:37 +0100

    fix

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -251,7 +251,7 @@ export default class Work extends Component {
                     >
                       <div className="blog-entry">
                         <a
-                          href="keyman.photography"
+                          href="https://keyman.photography"
                           className="blog-img"
                         >
                           <img
